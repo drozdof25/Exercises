d36217cd6d98113b576e71b6eca6196100b72b93
drozdof25 <drozdof25@gmail.com>
2019-09-23 11:16:40 +0300

    Интерактив для проверки упражнений

--- pythonworld/exe1.py
+++ pythonworld/exe1.py
@@ -9,3 +9,8 @@ def arithmetic(num1,num2,operation):
         return num1/num2
     else:
         return 'Неизвестная операция'
+num1 = float(input('Введите первое число: '))
+num2 = float(input('Введите второе число: '))
+operation = str(input('Операция: '))
+print('Ответ: ' + str(arithmetic(num1,num2,operation)))
+
--- pythonworld/exe2.py
+++ pythonworld/exe2.py
@@ -4,3 +4,9 @@ def is_year_leap(year):
     if year % 4 ==0 and year % 100 !=0:
         return True
     return False
+
+year = int(input('Введите год: '))
+if is_year_leap(year):
+    print(str(year) + ' - високосный год')
+else:
+    print(str(year) + ' - не високосный год')
