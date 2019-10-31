ea10b8bc93e8b1489457a206acaf782d67f5e29f
drozdof25 <drozdof25@gmail.com>
2019-10-31 13:59:26 +0300

    Простой пример выбора данных из веб ресурса

--- JoelGrass/kivy_html.py
+++ JoelGrass/kivy_html.py
@@ -7,5 +7,7 @@ soup = BeautifulSoup(html,'html5lib')
 tags= [a for a in soup('a')
       if 'api-kivy' in a.get('href').split('.')[0]
        and ['reference', 'internal'] == a.get('class',[])]
+api = dict()
 for tag in tags:
-    print(tag.text)
+    api[tag.get('href').split('.',maxsplit=1)[1].replace('.html','')] = tag.text
+print(api)
